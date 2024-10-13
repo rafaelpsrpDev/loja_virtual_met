d67d548c5e8f9ba6ba1c9a46211babe1ed8697b9
Rafael Paulo <rafaelpsrp@outlook.com>
2024-10-13 18:43:56 -0300

    criacao classe marca e categoria produto

--- src/main/java/com/lojavirtual/met/model/MarcaProduto.java
+++ src/main/java/com/lojavirtual/met/model/MarcaProduto.java
@@ -1,6 +1,7 @@
 package com.lojavirtual.met.model;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -42,5 +43,28 @@ public class MarcaProduto  implements Serializable {
 		public void setNomeDesc(String nomeDesc) {
 			this.nomeDesc = nomeDesc;
 		}
+
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(id);
+		}
+
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			MarcaProduto other = (MarcaProduto) obj;
+			return Objects.equals(id, other.id);
+		}
+
+
+		
+		
 		
 }
