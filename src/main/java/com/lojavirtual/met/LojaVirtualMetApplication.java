43bf3d4b9360b0beec450fd2b4f5c66e098c4289
Rafael Paulo <rafaelpsrp@outlook.com>
2024-10-12 21:31:23 -0300

    commit inicial projeto loja-virtual-met

---
+++ src/main/java/com/lojavirtual/met/LojaVirtualMetApplication.java
@@ -0,0 +1,13 @@
+package com.lojavirtual.met;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaVirtualMetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaVirtualMetApplication.class, args);
+	}
+
+}
