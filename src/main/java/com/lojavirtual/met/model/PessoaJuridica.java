f5e26302615c86d11aa5a85ddce009d3adf07317
Rafael Paulo <rafaelpsrp@outlook.com>
2024-10-13 21:05:31 -0300

    implementacao models (Acesso, Endereco, Pessoa, PessoaFisica, PessoaJuridica)

---
+++ src/main/java/com/lojavirtual/met/model/PessoaJuridica.java
@@ -0,0 +1,76 @@
+package com.lojavirtual.met.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.PrimaryKeyJoinColumn;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "pessoa_juridica")
+public class PessoaJuridica  extends Pessoa {
+
+	private static final long serialVersionUID = 1L;
+
+	@Column(nullable = false)
+	private String cnpj;
+	
+	private String inscEstadual;
+	
+	private String inscMunicipal;
+	
+	private String nomeFantasia;
+	
+	private String razaoSocial;
+	
+	private String descricao;
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	public String getInscEstadual() {
+		return inscEstadual;
+	}
+
+	public void setInscEstadual(String inscEstadual) {
+		this.inscEstadual = inscEstadual;
+	}
+
+	public String getInscMunicipal() {
+		return inscMunicipal;
+	}
+
+	public void setInscMunicipal(String inscMunicipal) {
+		this.inscMunicipal = inscMunicipal;
+	}
+
+	public String getNomeFantasia() {
+		return nomeFantasia;
+	}
+
+	public void setNomeFantasia(String nomeFantasia) {
+		this.nomeFantasia = nomeFantasia;
+	}
+
+	public String getRazaoSocial() {
+		return razaoSocial;
+	}
+
+	public void setRazaoSocial(String razaoSocial) {
+		this.razaoSocial = razaoSocial;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+}
